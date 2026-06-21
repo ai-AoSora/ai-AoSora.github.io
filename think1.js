229bb529d9e85fd3019bfc5c64e5a2c6ff07f397
bule-ttt <taiseisugawara4@gmail.com>
2026-06-21 18:08:39 +0900

    Update think1.js

--- think1.js
+++ think1.js
@@ -5,9 +5,9 @@ function send_question (input_text){
   for(let key in BasicDictionary){
   if(clear_input.includes(key)){
     const response = BasicDictionary[key];
-    const random_response = Math.floor(Math.random() * BasicDictionary.length);/* 辞書のリスト内からランダムで言葉選び */
+    const random_response = Math.floor(Math.random() * response.length);/* 辞書のリスト内からランダムで言葉選び */
     return response[random_response];/* 戻り値で選んだ言葉を戻す */
-    console.log(random_response)
+    console.log(random_response);/* テスト：コンソールに表示 */
     }
   }
   return　"404エラー"
