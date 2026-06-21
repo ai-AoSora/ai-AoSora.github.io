52aa9786e7b5659f148d8739a8870d90b403c415
bule-ttt <taiseisugawara4@gmail.com>
2026-06-21 17:56:23 +0900

    Fix variable name from clean_input to clear_input

--- think1.js
+++ think1.js
@@ -3,7 +3,7 @@ function send_question (input_text){
   const clear_input = input_text.trim();
   //辞書で元を見つける
   for(let key in BasicDictionary);
-  if(clean_input.includes(key)){
+  if(clear_input.includes(key)){
     const response = BasicDictionary[key];
     const random_response = Math.floor(Math.random() * BasicDictionary.length);/* 辞書のリスト内からランダムで言葉選び */
     return response[random_response];/* 戻り値で選んだ言葉を戻す */
